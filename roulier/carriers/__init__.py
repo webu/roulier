1e047c731fe5a16bc7706caca2cdfa1d22c335aa
Dylann Cordel <d.cordel@webu.coop>
2020-06-19 16:13:53 +0200

    wip

--- roulier/carriers/__init__.py
+++ roulier/carriers/__init__.py
@@ -2,13 +2,11 @@
 from . import laposte
 from . import dummy
 from . import geodis
-from . import dummy
 from . import dpd
 
 __all__ = [
     'laposte',
     'dummy',
     'geodis',
-    'dummy',
     'dpd',
 ]
